5a0a3625a2c284a6b6b85560c9f523a5718d5121
Edward Huang <ewhuang3@timan103.cs.illinois.edu>
2015-12-20 00:20:44 -0600

    Added to analyze_clusters.py rows for the densities of each cluster.

--- analyze_clusters.py
+++ analyze_clusters.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
         print 'Extracting cluster data...'
         # We use the "dirty" clusters with GO to analyze gene-GO edges.
         f = open('./results/clusters_%s_%s.txt' % (mode, run_num), 'r')
-        # Dictionary for clusters with GO. Keys are cluster indexes, values are the
-        # list of genes in those clusters.
+        # Dictionary for clusters with GO. Keys are cluster indexes, values are
+        # the list of genes in those clusters.
         clst_go_dct = {}
         for i, line in enumerate(f):
             if i == 0:
@@ -31,13 +31,13 @@ if __name__ == '__main__':
                 clst_go_dct[cluster] = [gene]
         f.close()
 
-        # We then read in the network to figure out how many GO terms went into the
-        # clustering.
+        # We then read in the network to figure out how many GO terms went into
+        # the clustering.
         print 'Extracting network data...'
         num_genes_net = 0
         num_gg_net = 0
         num_ggo_net = 0
-        f = open('./data/network_%s_%s.txt' % (mode, run_num), 'r')
+        f = open('./data/network_%s_1.txt' % (mode), 'r')
         edge_list_go = []
         for i, line in enumerate(f):
             print i
@@ -60,11 +60,24 @@ if __name__ == '__main__':
         num_ggo_net /= 2
         num_gg_net /= 2
 
+        # Get the output files of the Perl script evaluate_clustering.pl and
+        # find the in-density and out-density of each cluster.
+        dens_dct = {}
+        f = open('./results/cluster_eval_%s_%s.txt' % (mode, run_num), 'r')
+        for i, line in enumerate(f):
+            if line[:7] != 'Cluster':
+                continue
+            line = line.split()
+            clus_id, in_density, out_density = line[1], line[7], line[9]
+            dens_dct[clus_id] = (float(in_density), float(out_density))
+        f.close()
+
         print 'Writing out information...'
         # Write out to file.
         out = open('./results/clus_info_%s_%s.txt' % (mode, run_num), 'w')
         out.write('num_genes_in_net\tnum_g_g_net\tnum_g_go_net\n')
         out.write('%s\t%d\t%d\n' % (num_genes_net, num_gg_net, num_ggo_net))
+        out.write('in_dens\tout_dens\tin/(in+out)\t')
         out.write('num_genes\tnum_go_terms_in\tnum_g_g_edges\tnum_g_go_edges\n')
         for i in range(len(clst_go_dct)):
             cid = str(i + 1)
@@ -92,6 +105,9 @@ if __name__ == '__main__':
             assert(num_ggo % 2 == 0)
             num_gg /= 2
             num_ggo /= 2
+            in_dens, out_dens = dens_dct[cid]
+            ratio = in_dens / (in_dens + out_dens)
+            out.write('%g\t%g\t%g\t' % (in_dens, out_dens, ratio))
             out.write('%d\t%d\t%d\t%d\t' % (num_genes, num_go, num_gg, num_ggo))
             out.write('\t'.join(list(GO_terms)) + '\n')
         out.close()
