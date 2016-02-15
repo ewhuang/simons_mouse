ce65cc6062687b2ae1f4e07adb9a00c5cbe09c97
ewhuang <eddie.a7x@gmail.com>
2016-02-15 17:11:19 -0600

    Changed gene_gene_matrix.py to recompute pearson coefficients.

--- go_edge_prediction/gene_gene_matrix.py
+++ go_edge_prediction/gene_gene_matrix.py
@@ -1,35 +1,64 @@
 ### Author: Edward Huang
 
+from collections import OrderedDict
+import math
 
 ### Creates the top left block in the matrix to be sent to Sheng. Contains the
 ### gene-gene edge weights, ordered by appearance in original coexpression
 ### network.
 
+pearson_threshold = 0.8
+
+def average(x):
+    assert len(x) > 0
+    return float(sum(x)) / len(x)
+
+def pearsonr(x, y):
+    assert len(x) == len(y)
+    n = len(x)
+    assert n > 0
+    avg_x = average(x)
+    avg_y = average(y)
+    diffprod = 0
+    xdiff2 = 0
+    ydiff2 = 0
+    for idx in range(n):
+        xdiff = x[idx] - avg_x
+        ydiff = y[idx] - avg_y
+        diffprod += xdiff * ydiff
+        xdiff2 += xdiff * xdiff
+        ydiff2 += ydiff * ydiff
+    return diffprod / math.sqrt(xdiff2 * ydiff2)
+
 if __name__ == '__main__':
-    # First, read in the genes.
-    gene_lst = []
-    f = open('../data/full_network_genes.txt', 'r')
-    for line in f:
-        gene = line.strip()
-        gene_lst += [gene]
+    # Read in the tsv file.
+    print 'Reading in raw data...'
+    f = open('./data/mm_mrsb_log2_expression.tsv', 'r')
+    gene_exp_dct = OrderedDict({})
+    for i, line in enumerate(f):
+        if i == 0:
+            continue
+        line = line.split()
+        gene, exp_vals = line[0], line[1:]
+        exp_vals = [float(val) for val in exp_vals]
+        gene_exp_dct[gene] = exp_vals
     f.close()
 
-    # Read in the gene-gene edge weights.
-    edge_dct = {}
-    f = open('../data/full_network.txt', 'r')
-    for line in f:
-        gene_a, gene_b, weight = line.split()
-        gene_a = gene_lst.index(gene_a)
-        gene_b = gene_lst.index(gene_b)
-        edge_dct[(gene_a, gene_b)] = weight
-    f.close()
-
-    out = open('gene_gene_weights.txt', 'w')
-    for a, gene_a in enumerate(gene_lst):
-        for b, gene_b in enumerate(gene_lst):
-            if (a, b) in edge_dct:
-                out.write(edge_dct[(a, b)] + '\t')
-            else:
-                out.write(edge_dct([b, a]) + '\t')
+    # Calculate the correlations between each pair of genes.
+    print 'Computing Pearson correlation coefficients...'
+    out = open('./go_edge_prediction/gene_gene_weights.txt', 'w')
+    genes = gene_exp_dct.keys()
+    for a in range(len(genes)):
+        print '%f%% done...' % (float(a) / len(genes) * 100)
+        gene_a = genes[a]
+        exp_a = gene_exp_dct[gene_a]
+        for b in range(len(genes)):
+            gene_b = genes[b]
+            exp_b = gene_exp_dct[genes[b]]
+            pcc = str(abs(pearsonr(exp_a, exp_b)))
+            # If pearson is 1, then make no edge.
+            if pcc == 1.0:
+                pcc = 0.0
+            out.write(pcc + '\t')
         out.write('\n')
     out.close()
