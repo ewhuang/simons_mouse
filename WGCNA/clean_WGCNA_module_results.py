9cf89d1809dc0c580295e94dae597a531f138cc7
Edward Huang <ewhuang3@timan103.cs.illinois.edu>
2016-04-10 00:59:42 -0500

    Changed cleaning WGCNA results to take in sampled output.

--- WGCNA/clean_WGCNA_module_results.py
+++ WGCNA/clean_WGCNA_module_results.py
@@ -6,31 +6,32 @@
 
 
 if __name__ == '__main__':
-    # Find out what genes to keep.
-    f = open('./data/WGCNA_data/good_gene_booleans_WGCNA.txt', 'r')
-    good_gene_indices = []
-    for line in f:
-        good_gene_indices += [line.strip()]
-    f.close()
+    # # Find out what genes to keep.
+    # f = open('../data/WGCNA_data/good_gene_booleans_WGCNA.txt', 'r')
+    # good_gene_indices = []
+    # for line in f:
+    #     good_gene_indices += [line.strip()]
+    # f.close()
 
-    # Get the actual genes corresponding to the genes kept by WGCNA.
-    good_genes_lst = []
-    f = open('./data/mm_mrsb_log2_expression.tsv', 'r')
-    for i, line in enumerate(f):
-        # Skip header.
-        if i == 0:
-            continue
-        # Minus 1 because we didn't have a header for gg file.
-        gg_lst_index = i - 1
-        gg_bool = good_gene_indices[gg_lst_index]
-        if gg_bool == 'TRUE':
-            current_gene = line.split()[0]
-            good_genes_lst += [current_gene]
-    f.close()
+    # # Get the actual genes corresponding to the genes kept by WGCNA.
+    # good_genes_lst = []
+    # f = open('../data/mm_mrsb_log2_expression.tsv', 'r')
+    # for i, line in enumerate(f):
+    #     # Skip header.
+    #     if i == 0:
+    #         continue
+    #     # Minus 1 because we didn't have a header for gg file.
+    #     gg_lst_index = i - 1
+    #     print gg_lst_index
+    #     gg_bool = good_gene_indices[gg_lst_index]
+    #     if gg_bool == 'TRUE':
+    #         current_gene = line.split()[0]
+    #         good_genes_lst += [current_gene]
+    # f.close()
 
     # Extract all of the genes that appear in our randomly sampled network.
     sampled_network_genes = set([])
-    f = open('./data/network_no_go_20.txt', 'r')
+    f = open('../data/network_no_go_20.txt', 'r')
     for i, line in enumerate(f):
         if i < 2:
             continue
@@ -42,19 +43,20 @@ if __name__ == '__main__':
     # Find the module memberships for the relevant genes.
     gene_cluster_dct = {}
     module_lst = []
-    f = open('./data/WGCNA_data/module_membership_WGCNA.txt', 'r')
+    f = open('../data/WGCNA_data/module_membership_WGCNA.txt', 'r')
     for i, line in enumerate(f):
         # Skip header.
         if i == 0:
             continue
         # Columns are module, color, and module_membership. We ignore the last
         # two.
-        module, color, membership = line.split()
+        gene, module, color, membership = line.split()
         if module == '0':
             continue
         # Because we have a header.
-        good_gene_index = i - 1
-        gene = good_genes_lst[good_gene_index]
+        # good_gene_index = i - 1
+        # gene = good_genes_lst[good_gene_index]
+        gene = gene.strip('"')
         if gene not in sampled_network_genes:
             continue
         # Make sure each gene is only in one cluster at most.
@@ -65,7 +67,7 @@ if __name__ == '__main__':
     f.close()
 
     # Write an output file in the style of the simulated annealing.
-    out = open('./results/WGCNA_results/WGCNA_clusters_all_genes.txt', 'w')
+    out = open('../results/WGCNA_results/WGCNA_clusters_all_genes.txt', 'w')
     out.write('header\n')
     for gene in gene_cluster_dct:
         cluster_num = module_lst.index(gene_cluster_dct[gene]) + 1
