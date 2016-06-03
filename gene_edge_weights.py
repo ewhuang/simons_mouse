ae91324b22349fc49f8f2a0d61be782dc8791274
Edward Huang <ewhuang3@timan103.cs.illinois.edu>
2016-06-03 03:00:50 -0500

    Changed gene edge weights to use matrix operations when computing Pearson coffecient.

--- gene_edge_weights.py
+++ gene_edge_weights.py
@@ -1,7 +1,8 @@
 ### Author: Edward Huang
 
 import file_operations
-from scipy.stats import pearsonr
+import numpy as np
+from scipy.stats import pearsonr, betai
 import time
 
 ### This file generates the base file that we use to create the network with
@@ -9,48 +10,57 @@ import time
 ### finds the Pearson correlation coefficient between every pair of genes,
 ### making an edge in the generated network if the coefficient exceeds a 
 ### set threshold. Each line is "GENE_1\tGENE_2\tPEARSON_SCORE".
-### Run time: 2.6 hours.
+### Run time: 7 minutes.
 
 P_VALUE_THRESHOLD = 0.001
 
-def get_gene_to_index_dct(genes):
+def corrcoef(matrix):
     '''
-    Takes a list of genes, and returns a dictionary where keys are the genes,
-    and the values are their corresponding indices in the lists.
+    Received code from following link:
+    http://stackoverflow.com/questions/24432101/correlation-coefficients-and-p-v
+    alues-for-all-pairs-of-rows-of-a-matrix
     '''
-    gene_to_index_dct = {}
-    for index, gene in enumerate(genes):
-        gene_to_index_dct[gene] = str(index)
-    return gene_to_index_dct
+    r = np.corrcoef(matrix)
+    rf = r[np.triu_indices(r.shape[0], 1)]
+    df = matrix.shape[1] - 2
+    ts = rf * rf * (df / (1 - rf * rf))
+    pf = betai(0.5 * df, 0.5, df / (df + ts))
+    p = np.zeros(shape=r.shape)
+    p[np.triu_indices(p.shape[0], 1)] = pf
+    p[np.tril_indices(p.shape[0], -1)] = pf
+    p[np.diag_indices(p.shape[0])] = np.ones(p.shape[0])
+    return r, p
+
+def create_gene_exp_matrix(gene_exp_dct, high_std_genes):
+    gene_exp_matrix = []
+    for gene in high_std_genes:
+        gene_exp_matrix += [gene_exp_dct[gene]]
+    return np.array(gene_exp_matrix)
 
 def main():
     # Read in the tsv file.
     gene_exp_dct = file_operations.get_gene_expression_dct()
     high_std_genes = file_operations.get_high_std_genes()
 
-    # Map the gene ENSMUSG ID's to indices.
-    gene_to_index_dct = get_gene_to_index_dct(high_std_genes)
+    gene_exp_matrix = create_gene_exp_matrix(gene_exp_dct, high_std_genes)
 
-    # Calculate the correlations between each pair of genes.
-    out = open('./data/high_std_network.txt', 'w')
-    for a in range(len(high_std_genes)):
-        print '%f%% done...' % (float(a) / len(high_std_genes) * 100)
-        gene_a = high_std_genes[a]
-        exp_a = gene_exp_dct[gene_a]
-        gene_a_index = gene_to_index_dct[gene_a]
+    r, p = corrcoef(gene_exp_matrix)
 
-        for b in range(a + 1, len(high_std_genes)):
-            gene_b = high_std_genes[b]
-            exp_b = gene_exp_dct[high_std_genes[b]]
-            gene_b_index = gene_to_index_dct[gene_b]
+    out = open('./data/high_std_ensmusg_network.txt', 'w')
+    for row_index, row in enumerate(p):
+        for col_index, p_val in enumerate(row):
+            if col_index <= row_index or p_val > P_VALUE_THRESHOLD:
+                continue
 
-            # Compute correlation beween the two gene expression vectors.
-            pcc, p_value = pearsonr(exp_a, exp_b)
-
+            pcc = r[row_index][col_index]
             # Ignore genes that have PCC = 1 for the raw network.
-            if p_value < P_VALUE_THRESHOLD and pcc < 1.0:
-                out.write('%s\t%s\t%f\n' % (gene_a_index, gene_b_index,
-                    abs(pcc)))
+            if pcc == 1:
+                continue
+
+            # Write out gene information.
+            gene_a, gene_b = (high_std_genes[row_index],
+                high_std_genes[col_index])
+            out.write('%s\t%s\t%f\n' % (gene_a, gene_b, abs(pcc)))
     out.close()
 
 if __name__ == '__main__':
